023987973a6a32c992f3d1dd8f439b004c5753a6
Daniel Unapanta <dunapanta@outlook.es>
2023-04-26 21:32:23 -0500

    Add display section

---
+++ src/components/DisplaySection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const DisplaySection = () => {
+  return (
+    <div className="display-section wrapper">
+      <h2 className="title">New</h2>
+      <p className="text">Brillant</p>
+      <span className="description">
+        A display that's up to 2x brighter in the sun.
+      </span>
+      <button className="button">Try me!</button>
+      <button className="back-button">Top</button>
+    </div>
+  );
+};
