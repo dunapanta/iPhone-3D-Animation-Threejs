333db622387f142b4a4acfc6693d54a1ca238b5e
Daniel Unapanta <dunapanta@outlook.es>
2023-04-26 21:04:08 -0500

    Add navbar items

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Logo from "../assets/images/favicon.ico";
+import Search from "../assets/images/search.svg";
+import Store from "../assets/images/store.svg";
 
 export const Nav = () => {
   return (
@@ -8,6 +10,39 @@ export const Nav = () => {
         <ul className="list-styled">
           <li>
             <img src={Logo} alt="Logo" width={40} height={40} />
+          </li>
+          <li>
+            <a className="link-styled">Store</a>
+          </li>
+          <li>
+            <a className="link-styled">Mac</a>
+          </li>
+          <li>
+            <a className="link-styled">iPad</a>
+          </li>
+          <li>
+            <a className="link-styled">iPhone</a>
+          </li>
+          <li>
+            <a className="link-styled">Watch</a>
+          </li>
+          <li>
+            <a className="link-styled">Airpods</a>
+          </li>
+          <li>
+            <a className="link-styled">Accesories</a>
+          </li>
+          <li>
+            <a className="link-styled">Watch</a>
+          </li>
+          <li>
+            <a className="link-styled">Support</a>
+          </li>
+          <li>
+            <img src={Search} alt="Search" />
+          </li>
+          <li>
+            <img src={Store} alt="Store" />
           </li>
         </ul>
       </div>
